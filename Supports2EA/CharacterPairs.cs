4476fa3ae532dce268400017cd8f0e81c595d196
Sme <esahig15789@gmail.com>
2022-07-07 15:23:36 -0400

    Create CharacterPairs.cs

---
+++ Supports2EA/CharacterPairs.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Supports2EA { 
+
+	public class CharacterPairs
+	{
+
+		public string char1;
+		public string char2;
+		public CharacterPairs(string c1, string c2)
+		{
+			char1 = c1;
+			char2 = c2;
+		}
+	}
+}
