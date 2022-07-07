17ce69a68b000cd31c830d7bf0684ff0bace627a
Sme <esahig15789@gmail.com>
2022-07-06 22:49:59 -0400

    Update Character.cs

--- Supports2EA/Character.cs
+++ Supports2EA/Character.cs
@@ -4,10 +4,17 @@ namespace Supports2EA
 {
     public class Character
     {
-
-
+        byte index;
+        byte[] supportPartners = new byte[6];
+        byte[] initialValues = new byte[6];
+        byte[] growthRates = new byte[6];
+        byte numPartners = 0;
+        
+        public void init(byte id) 
+        {
+            this.index = id;
+        }
 
     }
-
 }
 
