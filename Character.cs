1b4d8382feb53cd7cd62279aa770b0266240ee95
Sme <esahig15789@gmail.com>
2022-07-07 16:15:28 -0400

    Update Character.cs

--- Character.cs
+++ Character.cs
@@ -6,9 +6,9 @@ namespace Supports2EA
     {
         public string name;
         public byte index;
-        public string[] supportPartners = new string[6];
-        public byte[] initialValues = new byte[6];
-        public byte[] growthRates = new byte[6];
+        public string[] supportPartners = new string[7];
+        public byte[] initialValues = new byte[7];
+        public byte[] growthRates = new byte[7];
         public byte numPartners = 0;
         
         public void init(string n, byte id) 
